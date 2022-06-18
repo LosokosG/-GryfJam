bc4cdc4c7a9e5d4b4146335ba9cf320b8e9fbb8f
Grymbi <47464968+Grymbi@users.noreply.github.com>
2022-06-18 18:57:24 +0200

    xd
    xd

--- gryfjam/Assets/Scripts/Bullet.cs
+++ gryfjam/Assets/Scripts/Bullet.cs
@@ -9,14 +9,14 @@ public class Bullet : MonoBehaviour
     [SerializeField] private TMP_Text scoreText;
     private void Awake()
     {
-        scoreText = GameObject.FindGameObjectWithTag("Score").GetComponent<TMP_Text>();
+        //scoreText = GameObject.FindGameObjectWithTag("Score").GetComponent<TMP_Text>();
     }
    
     private void OnCollisionEnter2D(Collision2D col)
     {
         
         Global.score += Random.Range(10, 50);
-        scoreText.text = $"SCORE [{Global.score}]";
+        //scoreText.text = $"SCORE [{Global.score}]";
         Destroy(gameObject);
 
 
